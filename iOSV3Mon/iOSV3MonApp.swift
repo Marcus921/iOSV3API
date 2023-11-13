e6d808e9c12ff4412dca4d7fd23fb0c8e6dbe141
Marcus921 <93766924+Marcus921@users.noreply.github.com>
2023-11-13 09:14:53 +0100

    Initial Commit

---
+++ iOSV3Mon/iOSV3MonApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSV3MonApp.swift
+//  iOSV3Mon
+//
+//  Created by Marcus Malmgren on 2023-11-13.
+//
+
+import SwiftUI
+
+@main
+struct iOSV3MonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
